3d8074797347fc5781372cc40f6a1d0d5e1ffb18
coderdevanshpro <devanshsharma75cp@gmail.com>
2025-01-27 22:37:51 +0530

    new commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       {loading ? (
         <Loading />
       ) : (
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 rounded-lg">
           <div className="px-4 py-6 sm:px-0">
             <div className="flex flex-wrap m-4">
               {pins && pins.length > 0 ? (
